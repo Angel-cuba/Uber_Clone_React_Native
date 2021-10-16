062378f065abc8ac7e4f3c10a602f1d25fe5b08d
Angel <araozangel842@gmail.com>
2021-10-16 21:45:47 +0300

    Working on it

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RideOptionsCard = () => {
+	return (
+		<View>
+			<Text>Ride options side</Text>
+		</View>
+	);
+};
+
+export default RideOptionsCard;
+
+const styles = StyleSheet.create({});
