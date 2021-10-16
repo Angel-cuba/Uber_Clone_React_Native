87bb77bfb65c9f33e6b49d48cc458ffc757eb7ae
Angel <araozangel842@gmail.com>
2021-10-16 21:25:13 +0300

    Using KeyBoarAvoidingView with properties

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreens from './screens/HomeScreens';
 import { store } from './store';
@@ -18,22 +18,28 @@ export default function App() {
 		<Provider store={store}>
 			<NavigationContainer>
 				<SafeAreaProvider>
-					<Stack.Navigator>
-						<Stack.Screen
-							name="HomeScreens"
-							component={HomeScreens}
-							options={{
-								headerShown: false,
-							}}
-						/>
-						<Stack.Screen
-							name="MapScreens"
-							component={MapScreens}
-							options={{
-								headerShown: false,
-							}}
-						/>
-					</Stack.Navigator>
+					<KeyboardAvoidingView
+						behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+						style={{ flex: 1 }}
+						keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+					>
+						<Stack.Navigator>
+							<Stack.Screen
+								name="HomeScreens"
+								component={HomeScreens}
+								options={{
+									headerShown: false,
+								}}
+							/>
+							<Stack.Screen
+								name="MapScreens"
+								component={MapScreens}
+								options={{
+									headerShown: false,
+								}}
+							/>
+						</Stack.Navigator>
+					</KeyboardAvoidingView>
 				</SafeAreaProvider>
 			</NavigationContainer>
 		</Provider>
