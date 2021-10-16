252c74d1d45cce8f1cebde8503f89518863af456
Angel <araozangel842@gmail.com>
2021-10-16 21:45:10 +0300

    Map view, second view component

--- screens/MapScreens.js
+++ screens/MapScreens.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import Map from '../components/Map';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
 
 const MapScreens = () => {
+	const Stack = createNativeStackNavigator();
 	return (
 		<View style={styles.container}>
-			<Text>MapScreens</Text>
+			<View style={tw`h-1/2`}>
+				<Map />
+			</View>
+			<View style={tw`h-1/2`}>
+				<Stack.Navigator>
+					<Stack.Screen
+						name="NavigateCard"
+						component={NavigateCard}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="RideOptionsCard"
+						component={RideOptionsCard}
+						options={{ headerShown: false }}
+					/>
+				</Stack.Navigator>
+			</View>
 		</View>
 	);
 };
@@ -13,7 +35,6 @@ export default MapScreens;
 
 const styles = StyleSheet.create({
 	container: {
-		backgroundColor: 'red',
 		height: '100%',
 	},
 });
