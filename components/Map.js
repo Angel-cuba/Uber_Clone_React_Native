44496d818a0c113c8758506adb277ef2e7877b72
Angel <araozangel842@gmail.com>
2021-10-17 16:01:07 +0300

    Fetching distance and duration

--- components/Map.js
+++ components/Map.js
@@ -4,13 +4,16 @@ import MapView, { Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
-import { selectOrigin, selectDestination } from '../slices/navSlice';
+import { selectOrigin, selectDestination, setTravelTimeInformations } from '../slices/navSlice';
 import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
 
 const Map = () => {
 	const origin = useSelector(selectOrigin);
 	const destination = useSelector(selectDestination);
 	const mapRef = useRef(null);
+
+	const dispatch = useDispatch();
 
 	// console.log('Origen----', origin.description);
 	//console.log(destination);
@@ -30,6 +33,22 @@ const Map = () => {
 	// 		},
 	// 	});
 	// }, [origin, destination]);
+	useEffect(() => {
+		if (!origin || !destination) return;
+
+		const getTravelTime = async () => {
+			fetch(
+				`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.destination}&key=${GOOGLE_MAPS_APIKEY}`
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					dispatch(setTravelTimeInformations(data.rows[0].elements[0]));
+					// console.log('Map.js', data.rows[0].elements[0].status);
+				});
+		};
+
+		getTravelTime();
+	}, [origin, destination, GOOGLE_MAPS_APIKEY]);
 
 	return (
 		<MapView
@@ -37,10 +56,10 @@ const Map = () => {
 			style={tw`flex-1`}
 			mapType="mutedStandard"
 			initialRegion={{
-				latitude: origin.location.lat,
-				longitude: origin.location.lng,
-				latitudeDelta: 0.05,
-				longitudeDelta: 0.05,
+				latitude: origin?.location.lat,
+				longitude: origin?.location.lng,
+				latitudeDelta: 0.5,
+				longitudeDelta: 0.5,
 			}}
 		>
 			{origin && destination && (
@@ -53,6 +72,7 @@ const Map = () => {
 						apikey={GOOGLE_MAPS_APIKEY}
 						strokeWidth={3}
 						strokeColor="#000"
+						lineDashPattern={[1]}
 					/>
 				</>
 			)}
