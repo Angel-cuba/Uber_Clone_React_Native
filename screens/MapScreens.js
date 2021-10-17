7bec972fb6249fc2fc89b7ecccf4cd623262f1e2
Angel <araozangel842@gmail.com>
2021-10-17 16:03:45 +0300

    Menu icon

--- screens/MapScreens.js
+++ screens/MapScreens.js
@@ -5,11 +5,19 @@ import Map from '../components/Map';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
 
-const MapScreens = () => {
+const MapScreens = ({ navigation }) => {
 	const Stack = createNativeStackNavigator();
 	return (
 		<View style={styles.container}>
+			<TouchableOpacity
+				onPress={() => navigation.navigate('HomeScreens')}
+				style={tw`bg-red-300 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+			>
+				<Icon name="menu" />
+			</TouchableOpacity>
 			<View style={tw`h-1/2`}>
 				<Map />
 			</View>
